3ffd51bb88491d7fc2c72c9fe3f730375249e68d
rodrigodiasb <62857786+rodrigodiasb@users.noreply.github.com>
2026-05-10 12:27:13 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,11 @@
-/*
-  CONFIGURAÇÃO DO FIREBASE
-  ------------------------------------------------------------
-  1. Cole aqui APENAS o objeto firebaseConfig gerado no console do Firebase.
-  2. Depois mude QUIZ_FIREBASE_ENABLED para true.
-  3. Não cole comandos npm, imports ou <script>...</script> dentro deste arquivo.
-*/
-
 window.QUIZ_FIREBASE_ENABLED = true;
 
 window.QUIZ_FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDPaBxhYUq78_3301jQqmAC1OEfElb_brM",
-    authDomain: "quiz-terminologias.firebaseapp.com",
-    projectId: "quiz-terminologias",
-    storageBucket: "quiz-terminologias.firebasestorage.app",
-    messagingSenderId: "1014542941866",
-    appId: "1:1014542941866:web:96657ac438eaf15dd85ad3",
-    measurementId: "G-KVW318G6NW"
+  apiKey: "AIzaSyDPaBxhYUq78_3301jQqmAC1OEfElb_brM",
+  authDomain: "quiz-terminologias.firebaseapp.com",
+  projectId: "quiz-terminologias",
+  storageBucket: "quiz-terminologias.firebasestorage.app",
+  messagingSenderId: "1014542941866",
+  appId: "1:1014542941866:web:96657ac438eaf15dd85ad3",
+  measurementId: "G-KVW318G6NW"
 };
